46f549451535cbe71d775c3e316ae0d0910f525d
C.Even <c.even@live.cn>
2022-03-16 11:37:46 +0800

    Version 1.0

--- addons/cherrysoda_spritebank_importer/cherrysoda_spritebank_import_plugin.gd
+++ addons/cherrysoda_spritebank_importer/cherrysoda_spritebank_import_plugin.gd
@@ -57,6 +57,45 @@ func get_option_visibility(option, options):
 	return true
 
 
+func get_frames(path):
+	var frames = []
+	var index = 0
+	var dir = Directory.new()
+	var single_file = "res://textures/%s.png" % [path]
+	if dir.file_exists(single_file):
+		return [single_file]
+	while true:
+		var s = str(index).length()
+		var found = false
+		for i in range(s, 5):
+			var format_string = "res://textures/%s%0" + str(i) + "d.png"
+			var file_name = format_string % [path, index]
+			if dir.file_exists(file_name):
+				frames.append(file_name)
+				found = true
+				break
+		if not found:
+			break
+		index += 1
+	return frames
+
+
+func get_animation(animatedSprite, id, fps, frame_count, loop):
+	var animation = Animation.new()
+	var start_track_id = animation.add_track(Animation.TYPE_VALUE)
+	animation.track_set_interpolation_type(start_track_id, Animation.INTERPOLATION_NEAREST)
+	animation.track_set_path(start_track_id, "AnimatedSprite:animation")
+	animation.track_insert_key(start_track_id, 0, id)
+	var track_id = animation.add_track(Animation.TYPE_VALUE)
+	animation.track_set_interpolation_type(track_id, Animation.INTERPOLATION_NEAREST)
+	animation.track_set_path(track_id, "AnimatedSprite:frame")
+	for i in frame_count:
+		animation.track_insert_key(track_id, i / fps, i)
+	animation.set_length(frame_count / fps)
+	animation.set_loop(loop)
+	return animation
+
+
 func import(source_file, save_path, options, r_platform_variants, r_gen_files):
 	var scene = Node2D.new()
 	scene.set_name("Sprites")
@@ -77,6 +116,7 @@ func import(source_file, save_path, options, r_platform_variants, r_gen_files):
 		var animatedSprite = AnimatedSprite.new()
 		var animationPlayer = AnimationPlayer.new()
 		var spriteFrames = SpriteFrames.new()
+		var useIdForDefaultAnimationPath = sprite.has("UseIdForDefaultAnimationPath")
 		animatedSprite.frames = spriteFrames
 		scene.add_child(node)
 		node.set_owner(scene)
@@ -88,6 +128,8 @@ func import(source_file, save_path, options, r_platform_variants, r_gen_files):
 		spriteFrames.remove_animation("default")
 		
 		var default_fps = round(1.0 / float(sprite.delay))
+		var start_animation = str(sprite.start)
+		var sprite_path = str(sprite.path)
 
 		if sprite.has("Anim"):
 			for anim in sprite.Anim:
@@ -97,6 +139,20 @@ func import(source_file, save_path, options, r_platform_variants, r_gen_files):
 				spriteFrames.add_animation(anim.id)
 				spriteFrames.set_animation_loop(anim.id, false)
 				spriteFrames.set_animation_speed(anim.id, fps)
+				var anim_path = ""
+				if useIdForDefaultAnimationPath:
+					anim_path = anim.id
+				if anim.has("path"):
+					anim_path = anim.path
+				var frames = get_frames(sprite_path + anim_path)
+				for frame in frames:
+					var texture = load(frame)
+					spriteFrames.add_frame(anim.id, texture)
+				var animation = get_animation(animatedSprite, anim.id, fps, frames.size(), false)
+				animationPlayer.add_animation(anim.id, animation)
+				if anim.id == start_animation:
+					animatedSprite.animation = anim.id
+					animationPlayer.current_animation = anim.id
 		
 		if sprite.has("Loop"):
 			for anim in sprite.Loop:
@@ -106,8 +162,21 @@ func import(source_file, save_path, options, r_platform_variants, r_gen_files):
 				spriteFrames.add_animation(anim.id)
 				spriteFrames.set_animation_loop(anim.id, true)
 				spriteFrames.set_animation_speed(anim.id, fps)
-		
-		
+				var anim_path = ""
+				if useIdForDefaultAnimationPath:
+					anim_path = anim.id
+				if anim.has("path"):
+					anim_path = anim.path
+				var frames = get_frames(sprite_path + anim_path)
+				for frame in frames:
+					var texture = load(frame)
+					spriteFrames.add_frame(anim.id, texture)
+				var animation = get_animation(animatedSprite, anim.id, fps, frames.size(), true)
+				animationPlayer.add_animation(anim.id, animation)
+				if anim.id == start_animation:
+					animatedSprite.animation = anim.id
+					animationPlayer.current_animation = anim.id
+
 
 	var packed_scene = PackedScene.new()
 	packed_scene.pack(scene)
